81aaea6dd5681ea0d48ad48354e572708375c131
3zcs <abdulaziz.aloshin@gmail.com>
2017-09-08 23:14:31 +0300

    update readme and movie file

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 ## Instlation 
+Fork this files using terminal <br />
 `$ git clone https://github.com/3zcs/MovieTrailerWebsite.git`<br />
+Then enter your folder `<br />
 `$ cd MovieTrailerWebsite`<br />
 
 ## Run project 
+Run this command in terminal and will generate file named `fresh_tomatoes.html`<br />
 `$ python entertainment`<br />
+then click on `fresh_tomatoes.html` and your web site will be opend<br />
 
 
 ## project structure
--- movie.py
+++ movie.py
@@ -1,9 +1,10 @@
 import webbrowser
 
 
+''' Movie class show each movie object '''
 class Movie:
     valid_ratings = ["G", "PG", "PG-13", "R"]
-
+    ''' constractur method take title, storyline, poster and trailer '''
     def __init__(self, movie_title, movie_storyline,
                  movie_poster_image, movie_trailer_youtube):
         self.title = movie_title
@@ -11,6 +12,7 @@ class Movie:
         self.poster_image_url = movie_poster_image
         self.trailer_youtube_url = movie_trailer_youtube
 
+    ''' show_trailer method return a trailer '''
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
 
